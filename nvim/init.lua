a7cdf1764a05cac969dbdf8da5008ff9c96f57f2
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2026-01-02 18:03:00 -0800

    Add key mappings to open terminal splits

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,8 @@ vim.opt.tabstop = 4       -- Number of spaces a tab represents
 vim.opt.shiftwidth = 4    -- Number of spaces for each indent
 vim.opt.expandtab = true  -- Convert tabs to spaces
 vim.opt.number = true
+
+-- Open terminal on right side: `Space + T + L`
+vim.keymap.set('n', '<leader>tl', ':leftabove vsplit | terminal<CR>')
+-- Open terminal on right side: `Space + T + R`
+vim.keymap.set('n', '<leader>tr', ':rightbelow vsplit | terminal<CR>')
