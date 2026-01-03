2fd28875977843fd42b2329780f773f5e692ef2a
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2026-01-02 17:57:49 -0800

    Configure Neovim settings and require config.lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,5 @@
-
+require("config.lazy")
+vim.opt.tabstop = 4       -- Number of spaces a tab represents
+vim.opt.shiftwidth = 4    -- Number of spaces for each indent
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.number = true
